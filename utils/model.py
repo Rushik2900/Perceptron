f2beb9cb30e1b1a833f2a5ba1356a640db134a7a
Rushik29 <rushik.p@exactspace.co>
2022-12-10 15:58:19 +0530

    logging added and progress bar(tqdm)

--- utils/model.py
+++ utils/model.py
@@ -1,9 +1,11 @@
 import numpy as np
+import logging
+from tqdm import tqdm
 
 class Perceptron:
     def __init__(self, eta, epochs):
         self.weights = np.random.randn(3)*1e-4
-        print(f"initial weights before training: {self.weights}")
+        logging.info(f"initial weights before training: {self.weights}")
         self.eta = eta
         self.epochs = epochs
     
@@ -16,22 +18,22 @@ class Perceptron:
         self.y=y
         
         X_with_bias = np.c_[self.X, -np.ones((len(self.X), 1))] # concatination
-        print(f"X with bias {X_with_bias}")
+        logging.info(f"X with bias {X_with_bias}")
         
-        for epoch in range(self.epochs):
-            print("--"*10)
-            print(f"for epoch: {epoch}")
-            print("--"*10)
+        for epoch in tqdm(range(self.epochs), total=self.epochs, desc="training the models"):
+            logging.info("--"*10)
+            logging.info(f"for epoch: {epoch}")
+            logging.info("--"*10)
             
             y_hat = self.activationFunction(X_with_bias, self.weights) # forward propagation
-            print(f"predicted value after forward pass: {y_hat}")
+            logging.info(f"predicted value after forward pass: {y_hat}")
             self.error = self.y - y_hat
-            print(f"error: {self.error}")
+            logging.info(f"error: {self.error}")
             self.weights = self.weights + self.eta * np.dot(X_with_bias.T, self.error) # backward propagation
-            print(f"updated weights after epoch: {epoch}/{self.epochs}")
-            print(self.weights)
-            print(self.total_loss())
-            print("####"*10)
+            logging.info(f"updated weights after epoch: {epoch}/{self.epochs}")
+            logging.info(self.weights)
+            logging.info(self.total_loss())
+            logging.info("####"*10)
             
     def predict(self, X):
         X_with_bias = np.c_[X, -np.ones((len(X), 1))] # concatination
@@ -39,5 +41,5 @@ class Perceptron:
     
     def total_loss(self):
         total_loss = np.sum(self.error)
-        print(f"total loss: {total_loss}")
+        logging.info(f"total loss: {total_loss}")
         return total_loss
